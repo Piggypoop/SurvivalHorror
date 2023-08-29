a12cdd966ce6ab13b0db670cfc17be9fcfb031db
LangzeL <langzel@student.unimelb.edu.au>
2023-08-29 21:49:32 +1000

    update camera

---
+++ testing/Assets/PlayerCameraController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float sensitivityX = 2.0f;
+    public float sensitivityY = 2.0f;
+    private float rotationX = 0.0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivityX;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivityY;
+
+        // Rotate the player around the Y-axis (yaw)
+        playerTransform.Rotate(Vector3.up * mouseX);
+
+        // Calculate the vertical rotation for the camera (pitch)
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90, 90);
+
+        // Apply the rotations to the camera and player
+        transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        playerTransform.rotation = Quaternion.Euler(0, playerTransform.rotation.eulerAngles.y, 0);
+    }
+}
