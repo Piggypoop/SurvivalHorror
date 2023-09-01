dc6375ea563297196e2c6cb0addc826e9cd683c4
LangzeL <langzel@student.unimelb.edu.au>
2023-09-01 13:51:50 +1000

    update gravity

--- testing/Assets/PlayerControlMovement.cs
+++ testing/Assets/PlayerControlMovement.cs
@@ -5,24 +5,34 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController cc;
-    public float moveSpeed;
-    public float jumpSpeed;
-    private Vector3 dir;
-    private float horizontalMove, verticalMove;
+    public float moveSpeed = 5f;
+    public float gravity = -9.81f;
+    private Vector3 velocity;  // Velocity vector to store gravity
+
     private void Start()
     {
         cc = GetComponent<CharacterController>();
     }
 
-    private void FixedUpdate()  // Using FixedUpdate for better physics behavior
+    private void Update()
     {
+        // Check if the character is grounded
+        if (cc.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;  // Small downward force to keep the player grounded
+        }
+
+        // Apply gravity
+        velocity.y += gravity * Time.deltaTime;
+
         // Get input from the player
         float horizontalMove = Input.GetAxis("Horizontal") * moveSpeed;
         float verticalMove = Input.GetAxis("Vertical") * moveSpeed;
 
         // Calculate movement direction based on input
-        dir = transform.forward * verticalMove + transform.right * horizontalMove;
-        cc.Move(dir * Time.deltaTime);
+        Vector3 dir = transform.forward * verticalMove + transform.right * horizontalMove;
 
+        // Apply the movement and gravity
+        cc.Move(dir * Time.deltaTime + velocity * Time.deltaTime);
     }
 }
