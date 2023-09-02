5e371927ad1328cd23424e1583e1c1f67ba94da9
Piggypoop <dtjy16@gmail.com>
2023-09-03 05:27:06 +1000

    update gameover menu
    update font, colour and add button

--- testing/Assets/Script/GameOverScreen.cs
+++ testing/Assets/Script/GameOverScreen.cs
@@ -1,10 +1,22 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameOverScreen : MonoBehaviour
 {
+    public static bool isInteracting = false;
     public void Setup (int score){
         gameObject.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        isInteracting = true;
+    }
+
+    public void RestartGame()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene("Map_Hosp1", LoadSceneMode.Single);
+        print("The button is working");
     }
 }
