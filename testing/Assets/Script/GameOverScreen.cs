121b79deecb03882682b3585899893e60262ed6e
Piggypoop <dtjy16@gmail.com>
2023-09-04 17:15:20 +1000

    remove map texture

--- testing/Assets/Script/GameOverScreen.cs
+++ testing/Assets/Script/GameOverScreen.cs
@@ -1,3 +1,4 @@
+/*
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
@@ -18,5 +19,49 @@ public class GameOverScreen : MonoBehaviour
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         SceneManager.LoadScene("Map_Hosp1", LoadSceneMode.Single);
         print("The button is working");
+    }
+}
+*/
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public static bool isInteracting = false;
+    public CharacterController playerController; // Reference to the CharacterController component
+    private Vector3 initialPlayerPosition; // Store the initial player position
+
+    private void Awake()
+    {
+        // Store the initial player position
+        initialPlayerPosition = playerController.transform.position;
+    }
+
+    public void Setup(int score)
+    {
+        gameObject.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        isInteracting = true;
+    }
+
+    public void RestartGame()
+    {
+        // Reset the player's position to the initial position
+        ResetPlayerPosition();
+
+        // Reload the scene
+        SceneManager.LoadScene("Map_Hosp1", LoadSceneMode.Single);
+
+        print("The button is working");
+    }
+
+    private void ResetPlayerPosition()
+    {
+        // Set the player's position to the initial position
+        playerController.transform.position = initialPlayerPosition;
     }
 }
